1cb340ca2d1e5f3384691dc7e55a34ee650daec9
Mike <Svaazz@users.noreply.github.com>
2015-12-19 23:45:12 +0100

    Create sketchLampara.ino

---
+++ sketchLampara.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+   pinMode(LED, OUTPUT); //establecemos 13 como salida
+   Serial.begin(9600); //iniciando Serial
+}
+ 
+void loop()
+{
+   if (Serial.available() > 0)
+   {
+      mssg = Serial.read(); //leemos el serial
+ 
+      if(mssg == 'e')
+      {
+         digitalWrite(13, HIGH); //si entra una 'e' encendemos
+      }
+      else if(mssg == 'a')
+      {
+         digitalWrite(13, LOW); //si entra una 'a' apagamos
+      }
+   }
+}
